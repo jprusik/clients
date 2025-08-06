56898a4a6ec902c7deb2f4cd9496eee6490d5356
Jonathan Prusik <jprusik@users.noreply.github.com>
2025-08-06 17:35:09 -0400

    Update cipher-context-menu-handler.ts

--- apps/browser/src/autofill/browser/cipher-context-menu-handler.ts
+++ apps/browser/src/autofill/browser/cipher-context-menu-handler.ts
@@ -1,6 +1,6 @@
 import { firstValueFrom } from "rxjs";
 
-// asefassegdrs
+// asefassegdr
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
 import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
