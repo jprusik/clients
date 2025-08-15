1362e4fbdd38505a8d899ac541edf41bf5ccbb5a
Jonathan Prusik <jprusik@users.noreply.github.com>
2025-08-15 16:25:00 -0400

    Update autofill-init.ts

--- apps/browser/src/autofill/content/autofill-init.ts
+++ apps/browser/src/autofill/content/autofill-init.ts
@@ -61,7 +61,7 @@ class AutofillInit implements AutofillInitInterface {
    * be called once when the content script is loaded.
    */
   init() {
-    // Test changes
+    // Test change
     this.setupExtensionMessageListeners();
     this.autofillOverlayContentService?.init();
     this.collectPageDetailsOnLoad();
